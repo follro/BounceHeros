e8e5decc26db34b904632dd32e2f21cf2cfb0d9b
follro <108092605+follro@users.noreply.github.com>
2025-09-14 21:06:04 +0900

    fix: 슬링샷 구현중

--- Assets/Scripts/CatchZoneHandler.cs
+++ Assets/Scripts/CatchZoneHandler.cs
@@ -1,87 +1,82 @@
 using System;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CatchZoneHandler : MonoBehaviour
 {
-    //임시 
-    public float catchZoneHeightRatio;
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private LayerMask catchAbleLayer;
 
-    [SerializeField] private InputAction screenTouch;
-    private Rect catchZone;
+    [Header("Input")]
+    [SerializeField] private InputAction pressScreen;
+    [SerializeField] private InputAction dragPostion;
 
-    private Vector2 startPoint;
-    [SerializeField]private Transform target;
+    [Header("Dragging Pos")]
+    [SerializeField] private Vector2 dragStartPos;
+    [SerializeField] private Vector2 dragEndPos;
+
+    
+    private BaseHero hero;
 
     private void Start()
     {
-        startPoint = Vector2.zero;
-        catchZone = new Rect(0, 0, Screen.width , Screen.height * catchZoneHeightRatio);
+        hero = null;
     }
 
     private void OnEnable()
     {
-        screenTouch.started += context => OnCatchStared(context.ReadValue<Vector2>());
-        screenTouch.performed += context => OnCatchPerformed(context.ReadValue<Vector2>());
-        screenTouch.canceled += context => OnCatchCanceld(context.ReadValue<Vector2>());
-        screenTouch.Enable();
+        pressScreen.started += (context) => ScreenPressedAction(context.ReadValueAsButton());
+        pressScreen.canceled += (context) => ScreenPressedAction(context.ReadValueAsButton());
 
+        dragPostion.started += (context) => dragStartPos = context.ReadValue<Vector2>();
+        dragPostion.performed += (context) => Dragging(dragStartPos, context.ReadValue<Vector2>());
+        dragPostion.canceled += (context) => dragEndPos = context.ReadValue<Vector2>();
+
+        pressScreen.Enable();
+        dragPostion.Disable();
     }
     private void OnDisable()
     {
-        //screenTouch.performed
-        screenTouch.Disable();   
+        pressScreen.started -= (context) => ScreenPressedAction(context.ReadValueAsButton());
+        pressScreen.canceled -= (context) => ScreenPressedAction(context.ReadValueAsButton());
+
+        dragPostion.started -= (context) => dragStartPos = context.ReadValue<Vector2>();
+        dragPostion.performed -= (context) => Dragging(dragStartPos, context.ReadValue<Vector2>());
+        dragPostion.canceled -= (context) => dragEndPos = context.ReadValue<Vector2>();
+
+        pressScreen.Disable();
+        dragPostion.Disable();
     }
 
-   /* private void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        //mouse는 임시
-        var mouse = Mouse.current;
-        if (mouse == null)
-            return;
-
-        if (mouse.leftButton.wasPressedThisFrame)
-        {
-            Vector2 touchScreenPosition = mouse.position.ReadValue();
-
-            if(catchZone.Contains(touchScreenPosition))
-            {
-                Vector3 touchWorldPosition = Camera.main.ScreenToWorldPoint(touchScreenPosition);
-                touchWorldPosition.z = 0;
-                
-                //이때 불릿 감지에 성공하면 그 오브젝트의 포지션을 리턴으로 받아 그 위치 기준으로 땡겨서 놓으면 다른 이벤트를 다시 작동 
-                
-            }
-        }
-    }*/
-
-    // 캐쳐존 핸들러의 역할 터치가 들어왔을때 다른 곳에 현재 터치된 위치에서 실행되야 될 함수들이 있으면 실행됨
-    // 만약 true였으면 땡겨서 쏘는 이벤트를 작동시킴
-
-    private void OnCatchStared(Vector2 touchScreenPos)
-    {
-       if (catchZone.Contains(touchScreenPos))
-       {
-            //공이 존안에 들어와있는지 검사하는걸 해야됨
-            //target setting 어디선가 가져와야됨
-
-            startPoint = touchScreenPos + target.position;
-       }
-       
+        hero = collision.GetComponent<BaseHero>();
     }
 
-
-    private void OnCatchPerformed(Vector2 dragScreenPos)
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        if(target != null)
-        {
+        hero = null;
+    }
 
+    private void ScreenPressedAction(bool isPressed)
+    {
+        if (isPressed)
+            dragPostion.Enable();
+        else
+        {
+            dragStartPos = dragEndPos = Vector2.zero;
+            dragPostion.Disable();
         }
     }
 
-    private void OnCatchCanceld (Vector2 endScreenPos)
+    private void Dragging(Vector2 start, Vector2 end)
     {
 
     }
+
+    private void DragEnd(Vector2 endPos)
+    {
+        dragEndPos = endPos;    
+    }
+
 }
