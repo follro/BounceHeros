a838d8712357a1c0c962bb4b06df84d538bed641
follro <108092605+follro@users.noreply.github.com>
2025-10-06 17:47:08 +0900

    /feat: GameFlowManager 내부 구조 추가중

--- Assets/Scripts/MainScene/Managers/GameFlowManger/GameFlowManager.cs
+++ Assets/Scripts/MainScene/Managers/GameFlowManger/GameFlowManager.cs
@@ -8,6 +8,12 @@ namespace BounceHeros
 {
     public class GameFlowManager : MonoBehaviour, IInitializable
     {
+        //각 상태에 필요한 것들을 만들어야됨
+        //1. 정지기능
+        //2. 웨이브 관리자 (웨이브 세팅 관련 정보들이 필요함)
+        //3. 씬 로더(특정 버튼에만 필요하므로 필요없을까나)
+        //4. 
+
         private GameFlowStateMachine stateMachine;
 
 
