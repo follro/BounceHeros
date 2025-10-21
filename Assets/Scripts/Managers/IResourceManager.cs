6f1098fea468597b50d62ce7c26d8693bfd89ed5
follro <108092605+follro@users.noreply.github.com>
2025-10-21 20:16:12 +0900

    feat: 매니저 관리 시스템 개발중

---
+++ Assets/Scripts/Managers/IResourceManager.cs
@@ -0,0 +1,13 @@
+using Cysharp.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public interface IResourceManager 
+{
+    public UniTask<T> LoadAsync<T>(string key) where T : Object;
+    public UniTask<T> LoadAsync<T>(AssetReference assetReference) where T : Object;
+    public void Release<T>(T loadedObject) where T : Object;
+
+}
---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SoundManager : MonoBehaviour 
+{
+    //private IResourceManager resourceManager;
+
+    [Header("BGM")]  
+    public AudioClip bgmClips;
+    public float bgmVolume;
+    private List<AudioSource> bgmSource;
+
+    [Header("SFX")]
+    public AudioClip sfxClips;
+
+    public void Initialize(IResourceManager resourceManager)
+    {
+
+    }
+}
